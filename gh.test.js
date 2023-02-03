3f27c5f7b6e5b9c3c87dbcdd806f0c8ed16414b9
marrinaanna <103364214+marrinaanna@users.noreply.github.com>
2023-02-03 14:10:06 +0300

    Add files via upload

---
+++ gh.test.js
@@ -0,0 +1,35 @@
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+});
+afterEach(() => {
+  page.close();
+});
+
+describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+  });
+  test("The h1 header content'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual("GitHub: Let’s build from here · GitHub");
+  },50000);
+
+  test("The first link attribute", async () => {
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    expect(actual).toEqual("#start-of-content");
+  },50000);
+
+  test("The page contains Sign in button", async () => {
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toContain("Get started with Team");
+  },50000);
+});
