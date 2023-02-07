0aa4bdef0037a815e00389f83655382b82e69ed1
marrinaanna <103364214+marrinaanna@users.noreply.github.com>
2023-02-07 18:07:09 +0300

    Add files via upload

--- gh.test.js
+++ gh.test.js
@@ -2,15 +2,16 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-});
+},50000);
 afterEach(() => {
   page.close();
-});
+},50000);
 
 describe("Github page tests", () => {
   beforeEach(async () => {
     await page.goto("https://github.com/team");
-  });
+  },50000);
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
@@ -31,5 +32,28 @@ describe("Github page tests", () => {
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
     expect(actual).toContain("Get started with Team");
+  },50000);
+
+  test("The h1 header content 1", async () => {
+    await page.goto("https://github.com/enterprise");
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual("Enterprise · A smarter way to work together · GitHub");
+  },50000);
+
+  test("The h1 header content 2", async () => {
+    await page.goto("https://github.com/pricing");
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual("Pricing · Plans for every developer · GitHub");
+  },50000);
+
+  test("The h1 header content 3", async () => {
+    await page.goto("https://github.com");
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual("GitHub: Let’s build from here · GitHub");
   },50000);
 });
